592b4544a9c075d6f523d28212fa93c1b1fc3e99
supercoolin <colin.evrard@uclouvain.be>
2023-06-02 17:23:37 +0200

    add WIP task_dir_to_TaskData

--- task_common.py
+++ task_common.py
@@ -51,7 +51,7 @@ def task_dir_validate(task_dir_path: Path) -> bool:
         logger.debug(f"task path {str(task_dir_path)} does not exist")
         return False
     #list task files
-    dir_content = os.listdir()
+    dir_content = os.listdir(task_dir_path)
     dir_files = {str(f).lower():f for f in dir_content if os.path.isfile(f)}
     dir_subdirs = {str(d).lower():d for d in dir_content if os.path.isdir(d)}
 
@@ -79,21 +79,83 @@ def task_dir_validate(task_dir_path: Path) -> bool:
 
     logger.debug(f"Validated task directory {str(task_dir_path)}")
     return True
-    
-    
-    
 
-     
-
-
-
-
-    
-    
-
-
-def task_dir_to_obj(task_dir_path: Path) -> TaskData:
+def task_dir_to_TaskData(task_dir_path: Path, build_script: Path=None, lib_dirs: list[Path]=None) -> TaskData:
     """
-    @brief: Use a task directory to parse a task into a task dictionary 
+    @brief: Use a task directory to parse a task into a task dictionary
+
+    @param task_dir_path: (Path) instance of a path object pointing to the target task directory to parse into an object
+
+    @return TaskData: The TaskData
     """
-    pass
+    if not task_dir_validate(task_dir_path):
+        logger.error(f"Invalid task directory: {task_dir_path}")
+        raise ValueError("Invalid argument")
+
+    TaskData_init_kwargs= {
+        'template': None,
+        'task': None,
+        'build_script': None,
+        'lib_dirs': None,
+        'annex': None
+    } 
+    
+    #validate build script
+    if build_script and os.path.isfile(build_script):
+        TaskData_init_kwargs['build_script'] = build_script
+    elif build_script:
+        logger.warning(f"Build script file {build_script} does not exist")
+    
+    #validate lib dirs
+    if lib_dirs:
+        valid_libs = []
+        for p in lib_dirs:
+            if os.path.isdir(p):
+                valid_libs.append(p)
+            else:
+                logger.warning(f"Library/include directory {p} does not exist")
+        TaskData_init_kwargs['lib_dirs'] = valid_libs
+    
+    #discover files and folders
+    task_dir_content = {str(content).lower():content for content in os.listdir(task_dir_path)}
+    task_dir_files = {name:path for name, path in task_dir_content.items() if os.path.isfile(path)}
+    task_dir_subdirs = {name:path for name, path in task_dir_content.items() if os.path.isdir(s)(path)}
+
+    #Guess the yaml extension to the task file and load it into a dict
+    task_file = task_dir_files['task.yaml'] if 'task.yaml' in task_dir_files else task_dir_files['task.yml']
+    with open(task_file, 'r') as f:
+        TaskData_init_kwargs['task'] = yaml.load(f)
+
+    #discover files and folders of the student directory
+    student_dir_content = {str(content).lower():content for content in os.listdir(os.path.join(task_dir_path, task_dir_content['student']))}
+    student_dir_files = {name:path for name, path in student_dir_content.items() if os.path.isfile(path)}
+    student_dir_subdirs = {name:path for name, path in student_dir_content.items() if os.path.isdir(s)(path)}
+
+    #retrieve the template file and the annex files
+    annex = []
+    for fname, fpath in student_dir_files.items():
+        splitted = fname.split('.')
+        #TODO USE REGEX IN CASE OF MULTIPLE EXTENSIONS
+        if len(splitted) > 1:
+            ext = splitted[1]
+            if ext == 'tpl':
+                TaskData_init_kwargs['template'] = fpath
+            elif fname != build_script:
+                annex.append(fpath)
+        elif fname != build_script:
+            annex.append(fpath)
+    if len(annex) > 0:
+        TaskData_init_kwargs['annex'] = annex
+    
+    return TaskData(**TaskData_init_kwargs)
+
+    
+
+    
+
+
+    
+    
+
+
+    
